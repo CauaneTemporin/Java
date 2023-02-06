4cacafa920c5bd0a0761ae12f8b0ba7d48115dff
CauaneTemporin <temporin.cauane@gmail.com>
2023-02-06 16:23:03 -0300

    Leitura de arquivos Bloco try-with-resources

--- 36 - Leitura de arquivos - Classes FileReader BufferedReader/src/application/Program.java
+++ 36 - Leitura de arquivos - Classes FileReader BufferedReader/src/application/Program.java
@@ -9,13 +9,9 @@ public class Program {
 	public static void main(String[] args) {
 
 		String path = "C:\\Users\\55119\\Downloads\\ac.txt";
-		FileReader fr = null;
-		BufferedReader br = null;
 
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
-
+		//Bloco try que declara um ou mais recursos, e garante que esses recursos serão fechados ao final do bloco
+		try (BufferedReader br = new BufferedReader(new FileReader(path))){
 			String line = br.readLine();
 
 			while (line != null) {
@@ -27,18 +23,5 @@ public class Program {
 		 catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
 		} 
-		finally {
-			try {
-				if (br != null) {
-					br.close();
-				}
-				if (fr != null) {
-					fr.close();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-
-		}
 	}
 }
