76e0be4f8bf15c8d57869149d4490f61f57ca782
CauaneTemporin <temporin.cauane@gmail.com>
2023-02-14 19:48:14 -0300

    Expressão lambda inline

--- 51 - Expressões Lambda - Predicate/src/application/Program.java
+++ 51 - Expressões Lambda - Predicate/src/application/Program.java
@@ -6,6 +6,7 @@ import java.util.Locale;
 import java.util.function.Predicate;
 
 import entities.Product;
+import util.ProductPredicate;
 
 public class Program {
 
@@ -18,11 +19,23 @@ public class Program {
 		list.add(new Product("Tv", 900.00));
 		list.add(new Product("Mouse", 50.00));
 		list.add(new Product("Tablet", 350.50));
-		list.add(new Product("HD Case", 80.90));
+		list.add(new Product("HD Case", 80.90));	
 		
-		Predicate<Product> pred = p -> p.getPrice() >= 100.0;	
+		//Opção 1
+		//list.removeIf(new ProductPredicate());
 		
-		list.removeIf(pred);
+		//Opção 2
+		//list.removeIf(Product::staticProductPredicate);
+		
+		//Opção 3
+		//list.removeIf(Product::nonStaticProductPredicate);
+		
+		//Opção 4
+		//Predicate<Product> pred = p -> p.getPrice() >= 100.0;
+		//list.removeIf(pred);
+		
+		//Opção 5
+		list.removeIf(p -> p.getPrice() >= 100.0);
 		
 		for(Product p : list) {
 			System.out.println(p);
