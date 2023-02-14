2bae785d4493552815a947f2d2d9f06fef83d0a9
CauaneTemporin <temporin.cauane@gmail.com>
2023-02-14 20:32:35 -0300

    Expressão lambda declarada

--- 52 - Expressões Lambda - Consumer/src/application/Program.java
+++ 52 - Expressões Lambda - Consumer/src/application/Program.java
@@ -3,9 +3,9 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 import entities.Product;
-import util.PriceUpdate;
 
 public class Program {
 public static void main (String[] args) {
@@ -19,8 +19,9 @@ public static void main (String[] args) {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));	
 		
+		Consumer<Product> cons = p -> p.setPrice(p.getPrice() * 1.1);
 		
-		list.forEach(Product::nonStaticPriceUpdate);
+		list.forEach(cons);
 		list.forEach(System.out::println);
 		 
 	}
