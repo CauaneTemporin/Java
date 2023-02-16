10fa2ffea428e845f94db6585995bd21c9bac3e0
CauaneTemporin <temporin.cauane@gmail.com>
2023-02-15 21:05:47 -0300

    Expressão lambda inline

--- 53- Expressões Lambda - Function/src/application/Program.java
+++ 53- Expressões Lambda - Function/src/application/Program.java
@@ -3,7 +3,6 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import entities.Product;
@@ -20,6 +19,21 @@ public static void main (String[] args) {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));	
 		
+		//Opção 1
+		//List<String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
+				
+		//Opção 2
+		//List<String> names = list.stream().map(Product::staticUpperCase).collect(Collectors.toList());
+				
+		//Opção 3
+		//List<String> names = list.stream().map(Product::nonStaticUpperCase).collect(Collectors.toList());
+
+		//Opção 4
+		//Function<Product, String> func = p -> p.getName().toUpperCase();
+		//List<String> names = list.stream().map(func).collect(Collectors.toList());
+
+
+		//Opção 5
 		List<String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
 		
 		 names.forEach(System.out::println);
