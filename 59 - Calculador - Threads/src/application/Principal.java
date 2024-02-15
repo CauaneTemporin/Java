03f365c8d99318899405c756658b754f6758380e
Cauane Temporin <temporin.cauane@gmail.com>
2024-02-15 10:01:59 -0300

    Calculador sem Thread

---
+++ 59 - Calculador - Threads/src/application/Principal.java
@@ -0,0 +1,11 @@
+package application;
+
+public class Principal {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		System.out.println("Isso é a thread main");
+
+		Thread.sleep(5000);
+	}
+}
