ad9d56879d51cf074390e9cd19c202a1d52874a9
CauaneTemporin <temporin.cauane@gmail.com>
2024-02-21 22:24:24 -0300

    Conexão entre cliente e servidor

---
+++ 64 - Servidor de tarefas - Threads/src/servidor/ServidorTarefas.java
@@ -0,0 +1,21 @@
+package servidor;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServidorTarefas {
+
+	public static void main(String[] args) throws Exception {
+
+		System.out.println("---Iniciando o servidor---");
+
+		ServerSocket servidor = new ServerSocket(12345);
+
+		while (true) {
+			Socket socket = servidor.accept();
+			System.out.println("Aceitando novo cliente na porta: " + socket.getPort());
+
+			Thread.sleep(20000);
+		}
+	}
+}
