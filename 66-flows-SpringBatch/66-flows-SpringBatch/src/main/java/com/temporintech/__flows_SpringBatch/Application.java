d33569aacd02021c2ec3963d79eeefc3f035e0a4
CauaneTemporin <temporin.cauane@gmail.com>
2024-06-02 18:38:49 -0300

    Add BatchConfig sem flow

---
+++ 66-flows-SpringBatch/66-flows-SpringBatch/src/main/java/com/temporintech/__flows_SpringBatch/Application.java
@@ -0,0 +1,13 @@
+package com.temporintech.__flows_SpringBatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ 66-flows-SpringBatch/66-flows-SpringBatch/src/test/java/com/temporintech/__flows_SpringBatch/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.temporintech.__flows_SpringBatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
