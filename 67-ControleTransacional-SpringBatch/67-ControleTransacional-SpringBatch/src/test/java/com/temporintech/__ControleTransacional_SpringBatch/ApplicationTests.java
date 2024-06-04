5e405057825ff08f51527e6a3c73dcc4727f7f92
CauaneTemporin <temporin.cauane@gmail.com>
2024-06-03 21:34:20 -0300

    Add projeto de Controle Transacional

---
+++ 67-ControleTransacional-SpringBatch/67-ControleTransacional-SpringBatch/src/test/java/com/temporintech/__ControleTransacional_SpringBatch/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.temporintech.__ControleTransacional_SpringBatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
