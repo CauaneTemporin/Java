b98c647f82ef65ac80217cfeb9fa5934f517b7f6
CauaneTemporin <temporin.cauane@gmail.com>
2024-07-02 07:26:45 -0300

    Model Abrigo

---
+++ 68-pet-BoasPraticas/68-pet-BoasPraticas/src/main/java/com/temporintech/__pet_BoasPraticas/model/Abrigo.java
@@ -0,0 +1,40 @@
+package com.temporintech.__pet_BoasPraticas.model;
+
+public class Abrigo {
+
+    public Abrigo() {
+
+    }
+
+    public Abrigo(String nome, String telefone, String email) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    private Long id;
+    private String nome;
+    private String telefone;
+    private String email;
+    private Pet[] pets;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Pet[] getPets() {
+        return pets;
+    }
+}
