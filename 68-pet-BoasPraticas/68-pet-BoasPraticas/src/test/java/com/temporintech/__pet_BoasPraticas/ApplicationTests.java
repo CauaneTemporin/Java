96e62f4c8c49366dfb20e94a4d35c6d3970da531
CauaneTemporin <temporin.cauane@gmail.com>
2024-07-01 21:29:51 -0300

    Projeto inicial

---
+++ 68-pet-BoasPraticas/68-pet-BoasPraticas/src/test/java/com/temporintech/__pet_BoasPraticas/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.temporintech.__pet_BoasPraticas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
