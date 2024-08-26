52277729020e99ac59a053e93e279aa9486207db
cauane.temporin-at-422501651743 <cauane.temporin-at-422501651743>
2024-08-26 11:18:24 -0300

    Criação do projeto do map struct

---
+++ 69-UsuarioDados-Mapstruct/src/main/java/com/temporintech/__UsuarioDados_Mapstruct/Application.java
@@ -0,0 +1,13 @@
+package com.temporintech.__UsuarioDados_Mapstruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
