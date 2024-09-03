40c4de623e36e7d98c5718514fff2156565cbb37
CauaneTemporin <temporin.cauane@gmail.com>
2024-09-03 18:40:51 -0300

    Adicionando método commandLineRunner

--- 69-UsuarioDados-Mapstruct/src/main/java/com/temporintech/__UsuarioDados_Mapstruct/Application.java
+++ 69-UsuarioDados-Mapstruct/src/main/java/com/temporintech/__UsuarioDados_Mapstruct/Application.java
@@ -10,4 +10,22 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(UsuarioMapper usuarioMapper) {
+		return args -> {
+			UsuarioRequest usuarioRequest = UsuarioRequest.
+					builder().
+					nome("Matheus").
+					email("matheus@dev.com").
+					senha("123").
+					build();
+
+			UsuarioEntity usuarioEntity = usuarioMapper.map(usuarioRequest);
+			usuarioEntity.setId(1);
+			usuarioEntity.setDataCriacao(LocalDateTime.now());
+
+			UsuarioResponse usuarioResponse = usuarioMapper.map(usuarioEntity);
+			System.out.println(usuarioResponse);
+		};
+	}
 }
